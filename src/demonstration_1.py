abe651e7dccb29fef27dda7d9c8cbc53a3bc557d
Virginia P. Scirrotto <c0d3.vp@gmail.com>
2020-12-03 16:27:51 -0700

    finished project

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -23,4 +23,22 @@ if there is a mathematical approach that you can take.*
 """
 def to_lower_case(string):
     # Your code here
+    ascii_values = [ord(l) for l in string] # O(n) time, O(n) space 
+​
+    for i, v in enumerate(ascii_values): # O(n) time 
+​
+    # for i in range(len(ascii_values)):
+    #     v = ascii_values[i]
+​
+        if 65 >= v and v <= 90: # O(1)
+            # lowercasing the letter 
+            ascii_values[i] += 32 # O(1)
+    
+    # turn the ascii values back into a string 
+    # letters = [chr(v) for v in ascii_values]
+​
+    return ''.join(chr(v) for v in ascii_values) # O(n) time
+​
+​
+print(to_lower_case("LambdaSchool"))
 
